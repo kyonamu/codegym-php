509aab86ee8bf48bb0795c55e50058b1c593d1e4
小川　木楠 <>
2021-04-24 10:10:42 +0900

    php課題2

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,11 +1,17 @@
 <?php
 $array = explode(',', $_GET['array']);
 
-// 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+  for ($a = 1; $a < $count; $a++) {
+    $b = $a - 1;
+    if ($array[$a] < $array[$b]) {
+      $temp = $array[$b];
+      $array[$b] = $array[$a];
+      $array[$a] = $temp;
+    }
+  }
 }
-// 修正はここまで
 
 echo "<pre>";
 print_r($array);
